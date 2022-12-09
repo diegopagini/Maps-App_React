6c5e23315ce156c692f191d58cf1d9222444b2ef
Diego Pagini <diegopaginidev@gmail.com>
2022-12-09 15:26:58 +0100

    print line

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,5 +1,5 @@
 /** @format */
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from 'mapbox-gl';
 import { useContext, useEffect, useReducer } from 'react';
 
 import { directionsApi } from '../../apis';
@@ -50,13 +50,58 @@ export const MapProvider = ({ children }: Props) => {
 
 	const getRouteBetweenPoins = async (start: [number, number], end: [number, number]) => {
 		const resp = await directionsApi.get<Directions>(`/${start.join(',')};${end.join(',')}`);
-		const { distance, duration, geometry } = resp.data.routes[0];
-		let kms = distance / 1000;
-		kms = Math.round(kms * 100);
-		kms /= 100;
+		const { geometry } = resp.data.routes[0];
+		const { coordinates: coords } = geometry;
 
-		const minutes = Math.floor(duration / 60);
-		console.log({ kms, minutes, geometry });
+		const bounds = new LngLatBounds(start, start);
+
+		for (const coord of coords) {
+			const newCoord: [number, number] = [coord[0], coord[1]];
+			bounds.extend(newCoord);
+		}
+
+		state.map?.fitBounds(bounds, {
+			padding: 200,
+		});
+
+		// Polyline:
+		const sourceData: AnySourceData = {
+			type: 'geojson',
+			data: {
+				type: 'FeatureCollection',
+				features: [
+					{
+						type: 'Feature',
+						properties: {},
+						geometry: {
+							type: 'LineString',
+							coordinates: coords,
+						},
+					},
+				],
+			},
+		};
+
+		if (state.map?.getLayer('RouteString')) {
+			state.map.removeLayer('RouteString');
+			state.map.removeSource('RouteString');
+		}
+
+		// Paint route:
+		state.map?.addSource('RouteString', sourceData);
+		state.map?.addLayer({
+			id: 'RouteString',
+			type: 'line',
+			source: 'RouteString',
+			layout: {
+				'line-cap': 'round',
+				'line-join': 'round',
+			},
+			paint: {
+				'line-color': 'black',
+				'line-width': 3,
+			},
+		});
 	};
 
 	const setMap = (map: Map) => {
